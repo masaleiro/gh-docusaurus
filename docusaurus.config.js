9de13852941a6e534a9a72203671a842b1365a01
masaleiro <mariosaleiro@gmail.com>
2023-02-22 22:57:38 +0000

    Changed tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'gh-docusaurus',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Dinosaurs are super cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
